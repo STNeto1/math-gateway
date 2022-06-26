adbbc5e704eb14d51a745c943f00e3333155e42f
STNeto1 <netofreitas7052@gmail.com>
2022-06-26 19:45:52 -0300

    feat: timeout

--- apps/gateway/src/gateway.controller.ts
+++ apps/gateway/src/gateway.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, timeout } from 'rxjs';
 import { ResultOutput } from './dto/result.output';
 import { SumDto } from './dto/sum.dto';
 
@@ -12,9 +12,10 @@ export class GatewayController {
   async math(@Body() data: SumDto): Promise<ResultOutput<number>> {
     const pattern = { cmd: 'sum' };
 
-    const result = await lastValueFrom(
-      this.client.send<number>(pattern, data.numbers),
-    );
+    const observable = this.client
+      .send<number>(pattern, data.numbers)
+      .pipe(timeout(500));
+    const result = await lastValueFrom(observable);
 
     return {
       success: true,
